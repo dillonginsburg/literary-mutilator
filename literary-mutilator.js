9d8f719ed0e2e0522993b2985178fe66b9acf905
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-05-19 21:36:56 -0600

    having some troubles with .iea/ again...

---
+++ literary-mutilator.js
@@ -0,0 +1,10 @@
+function tier1(){
+	document.getElementById("p1").style.color = "#FF0000";
+	document.getElementById("p1").style.fontFamily = "Comic Sans MS";
+	document.getElementById("p1").style.backgroundColor = "yellow"
+}
+
+function tier2() {
+	document.body.innerHTML = document.body.innerHTML.replace("Lorem ipsum dolor sit amet, consectetur adipiscing elit", "This sentence is real now");
+	window.find("This is a real sentence, now.");
+}
